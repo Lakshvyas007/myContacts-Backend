53a319234134f3edd669160479dee3f8cbcf91cf
Lakshya Vyas <lakshyavyas987@gmail.com>
2023-08-15 22:39:25 +0530

    oneRouteDone

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use("/api/contacts", require("./routes/contactRoute"))
+
 app.listen(port, ()=>{
     console.log(`server listening on port ${port}`);
 });
